1ef9ccd782bae36679c57451d478d8a6f7cd2794
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-10-01 15:52:51 -0300

    exe2

--- src/A290-exercicio/form-control.ts
+++ src/A290-exercicio/form-control.ts
@@ -1,5 +1,3 @@
-import { FORMERR } from 'dns';
-
 const SHOW_ERROR_MESSAGES = 'show-error-message';
 
 const form = document.querySelector('.form') as HTMLFormElement;
@@ -11,14 +9,22 @@ const password2 = document.querySelector('.password2') as HTMLInputElement;
 form.addEventListener('submit', function (event: Event) {
   event.preventDefault();
   hideErrorMenssages(this);
+  checkFprEmptyFields(username, email, password, password2);
 });
 
-function hideErrorMenssages(form: HTMLFormElement): void;
-form
-  .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
-  .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+function checkFprEmptyFields(...inputs: HTMLFormElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value) showErrorMessage(input, 'Campo não pode ficar vazio');
+  });
+}
 
-function showError(input: HTMLInputElement, msg: string): void {
+function hideErrorMenssages(form: HTMLFormElement): void {
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
   const formFields = input.parentElement as HTMLSpanElement;
   const errorMessage = formFields.querySelector(
     '.error-message',
