d674be4b33b3cdf723af31158d4c7c758aa10cc1
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-10-08 17:25:44 -0300

    300

--- src/A300-atribuito-e-metodos-estaticos/index.ts
+++ src/A300-atribuito-e-metodos-estaticos/index.ts
@@ -1,11 +1,24 @@
 export class Pessoa {
+  static idadePadrao = 0;
+  static cpfPadrao = '000.000.000-00';
+
   constructor(
     public nome: string,
     public sobrenome: string,
     public idade: number,
     public cpf: string,
   ) {}
+  metodoNormal(): void {
+    console.log(Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
+
+  static criaPessoa(nome: string, sobrenome: string): Pessoa {
+    return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
 }
 
-const pessoa = new Pessoa('Bruno', 'Eduardo', 31, '123.456.414-12');
-pessoa.cpf = '123.456.414-44';
+const pessoa1 = new Pessoa('Bruno', 'Eduardo', 31, '123.456.414-12');
+const pessoa2 = Pessoa.criaPessoa('Bruno', 'Eduardo');
+
+console.log(pessoa1);
+console.log(pessoa2);
