09e0d5507c6e3efc1a642bf7b7809dca3d4de878
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-10-09 20:18:19 -0300

    Create index.ts

---
+++ src/A302-classes-metodos-abstratos/index.ts
@@ -0,0 +1,41 @@
+export abstract class Personagem {
+  private abstract emoji: string;
+  constructor(
+    protected nome: string,
+    protected ataque: number,
+    protected vida: number,
+  ) {}
+
+  atacar(personagem: Personagem): void {
+    // console.log(`${this.nome} está atacando.`);
+    this.bordao();
+    personagem.perderVida(this.ataque);
+  }
+  perderVida(forcaAtaque: number): void {
+    this.vida -= forcaAtaque;
+    console.log(`${this.emoji} - ${this.nome} agora tem ${this.vida} de vida`);
+  }
+  abstract bordao(): void;
+}
+
+export class Guerreira extends Personagem {
+  protected emoji = '\u{1F9DD}';
+  bordao(): void {
+    console.log(this.emoji + 'Sou guerreira e estou atacando kkkk');
+  }
+}
+export class Monstro extends Personagem {
+  protected emoji = '\u{1F9Df}';
+  bordao(): void {
+    console.log(this.emoji + 'Sou guerreiro e estou atacando kkkk');
+  }
+}
+
+const guerreira = new Guerreira('guerreira', 100, 1000);
+const mostro = new Monstro('mostro', 87, 1000);
+
+guerreira.atacar(mostro);
+guerreira.atacar(mostro);
+mostro.atacar(guerreira);
+mostro.atacar(guerreira);
+mostro.atacar(guerreira);
